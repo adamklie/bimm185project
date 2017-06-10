335b673bd275aa5cc41d1389483a01a99e173259
Adam <aklie@ucsd.edu>
2017-06-10 16:10:59 -0700

    got velvet working

--- SeqPro.py
+++ SeqPro.py
@@ -7,7 +7,8 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument("-f1", "--fastq1", required=True, help="forward fastq reads from bacterial sample", type=str)
 parser.add_argument("-f2", "--fastq2", required=False, help="reverse fastq reads from bacterial sample    ", type=str)
-parser.add_argument("-rt", "--read_type", required=True, help="type of read library input, s=single end, p=paired end, m=mate pair")
+parser.add_argument("-rt", "--read_type", required=True, help="type of read library input, s=single end, p=paired end, m=mate pair", type=str)
+parser.add_argument("-i", "--insert_size", required=False, help="insert size for paired end assembly", type=str)
 parser.add_argument("-r", "--reference", help="optional reference fasta to perform assembly against", required=False, type=str)
 parser.add_argument("-v", "--verbosity", action="store_true", help="increase output verbosity")
 pair = False
@@ -23,20 +24,28 @@ if args.read_type in "pm":
 	if args.fastq2 is None:
 		print("read pair not specified, exiting...")
 		sys.exit()
-
-#spades assembly with no reference
-if args.reference is None:
+"""
+#velvet assembly with no reference
+if args.reference is None: 
 	print("No reference given, performing de novo assembly")
 	if args.read_type == "s":
 		print("single end type assembly")
+		subprocess.call(["velveth", "velvet_out", "19", "-fastq", "-short", args.fastq1, args.fastq2], stderr=log)
+		subprocess.call(["velvetg", "velvet_out"], stderr=log)	
 	elif args.read_type == "p":
+		if args.insert_size is None:
+			print("must specify insert size for paired end assembly, exiting...")
+			sys.exit()
 		print("paired end type assembly")
+		subprocess.call(["velveth", "velvet_out", "19", "-fastq", "-shortPaired", args.fastq1, args.fastq2], stderr=log)
+		subprocess.call(["velvetg", "velvet_out", "-ins_length", args.insert_size, "-exp_cov", "auto",], stderr=log)  
 	elif args.read_type == "m":
-		print("mate pair type assembly")
+		print("don't have functionality built in yet, exiting...")
+		sys.exit()
 	else:
 		print("unrecognizable pair type, exiting...")
 		sys.exit()
-"""
+
 #reference based assembly with reference given
 else:
 	print("reference given, performing reference based assembly")
@@ -68,6 +77,6 @@ log.write(p.stderr.read())
 fastqfile.close()
 
 subprocess.call(["seqret", "-osformat", "fasta", "consensus_alignment.fq", "-out2", "consensus_alignment.fa"], stderr=log) 
-"""
 subprocess.call(["prokka", "consensus_alignment.fa", "--rnammer"), stderr=log]
+"""
 #subprocess.call("rm " + args.reference + ".*", shell=True)
